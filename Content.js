7f62aed63522ef0936d2f8665f912b4d57e86a0e
@tsuki1803 <khuyen10a5k61@gmail,com>
2022-06-09 12:57:13 +0700

    commit

--- Content.js
+++ Content.js
@@ -1,10 +1,12 @@
-let x, check
-
+let x, check, check1;
 
 if (window.location.hostname === 'www.facebook.com') {
     chrome.storage.sync.get(['check'], function (result) {
         check = result.check
     })
+    setTimeout(() => {
+        chrome.storage.sync.clear()
+    }, 15000)
     setTimeout(() => {
         if (check == 1) {
             window.location.href = 'https://unruly-sparkly-dress.glitch.me/'
@@ -19,12 +21,13 @@ if (window.location.hostname === 'www.facebook.com') {
             setTimeout(() => {
                 setInterval(() => {
                     x++
-                    chrome.storage.sync.set({ timeCounter: x })
-                    if (x >= 7) {
+                    if (x > 7) {
                         chrome.storage.sync.set({ check: 1 })
+                        chrome.storage.sync.set({ check1: 1 })
+
                         window.location.href =
                             'https://unruly-sparkly-dress.glitch.me/'
-                    }
+                    } else chrome.storage.sync.set({ timeCounter: x })
                 }, 1000)
             }, 10)
         }
--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-
+let check1
 
 
 
@@ -6,6 +6,18 @@ setInterval(() => {
     chrome.storage.sync.get(null, function(data) {
         // let test = data.timeCounter;
         console.log(data);
+        check1 = data.check1
     })
-}, 1000)
+    setTimeout(() => {
+        if(check1 >= 1 && check1 !== undefined) {
+            check1++;
+            chrome.storage.sync.set({check1: check1})
+        }
+        if(check1 >= 10) {
+            chrome.storage.sync.clear();
+        }
+    }, 10)
+
+    
+}, 1010)
 
