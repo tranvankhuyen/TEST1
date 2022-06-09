39ee02c0bca7e0090d9c12434bb328c565b9e772
@tsuki1803 <khuyen10a5k61@gmail,com>
2022-06-09 10:32:45 +0700

    commit

--- Content.js
+++ Content.js
@@ -1,8 +1,9 @@
-let x, checkTimeBlock
+let x, check
+
 
 if (window.location.hostname === 'www.facebook.com') {
-    chrome.storage.sync.get(['checkTimeBlock'], function (result) {
-        checkTimeBlock = result.check
+    chrome.storage.sync.get(['check'], function (result) {
+        check = result.check
     })
     setTimeout(() => {
         if (check == 1) {
@@ -19,13 +20,15 @@ if (window.location.hostname === 'www.facebook.com') {
                 setInterval(() => {
                     x++
                     chrome.storage.sync.set({ timeCounter: x })
-
-                    if (x >= 7) {                
-                        chrome.storage.sync.set({ checkTimeBlock: 1 })
-                        window.location.href = 'https://unruly-sparkly-dress.glitch.me/'
+                    if (x >= 7) {
+                        chrome.storage.sync.set({ check: 1 })
+                        window.location.href =
+                            'https://unruly-sparkly-dress.glitch.me/'
+                        chrome.storage.sync.set({ timeCounter: 0 })
+                        setInterval.clear();
                     }
                 }, 1000)
-            }, 0)
+            }, 10)
         }
     }, 10)
 }
